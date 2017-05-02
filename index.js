9637b00a81b6c898e4c8eb519fe0e460ff39db31
Jose Emmanuel Manangan <noel1231@users.noreply.github.com>
2017-05-02 14:06:27 +0800

    initial files

---
+++ index.js
@@ -0,0 +1,14 @@
+class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Main />,
+    document.getElementById('root')
+);
