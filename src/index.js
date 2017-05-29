a56784fe29a8aa9e3dd231ae5f6bda2f0240c8e9
Roel Rosil <roelrosil1705@gmail.com>
2017-05-29 13:40:30 +0800

    Separation of css for responsive

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import './css/320-480.css';
+import './css/481-566.css';
+import './css/567-939.css';
+import './css/940-1023.css';
+import './css/1024-1219.css';
+import './css/1220-1582.css';
+import './css/1583.css';
 
 ReactDOM.render(
   <App />,
