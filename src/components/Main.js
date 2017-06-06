2347015377e5e95665a5d9c0f444b3508b1c699e
Roel Rosil <roelrosil1705@gmail.com>
2017-06-06 18:09:28 +0800

    CW3 CW7 CW9

--- src/components/Main.js
+++ src/components/Main.js
@@ -52,12 +52,6 @@ class Main extends Component {
                 <img className="hide-temp shapes rotate polygon2" src="./img/shapes/polygon-2.png"/>
                 {/*<img className="hide-temp shapes rectangle6" src="./img/shapes/rectangle-6.png"/>*/}
                 <img className="hide-temp shapes rotate rectangle4" src="./img/shapes/rectangle-4.png"/>
-                <img className="hide-temp shapes rotate polygon22" src="./img/shapes/polygon-2.png"/>
-                <img className="hide-temp shapes rotate polygon33" src="./img/shapes/polygon-3.png"/>
-                <img className="hide-temp shapes rotate ellipse22" src="./img/shapes/ellipse-2.png"/>
-                <img className="hide-temp shapes rotate polygon44" src="./img/shapes/polygon-4.png"/>
-                <img className="hide-temp shapes rotate rectangle1" src="./img/shapes/rectangle.png"/>
-                <img className="hide-temp shapes rotate rectangle2" src="./img/shapes/rectangle-2.png"/>
 
 
                 <div className="mdl-grid webcover" id="home">
@@ -66,7 +60,7 @@ class Main extends Component {
                         <h1 className="newweb" style={{fontFamily: 'MerriweatherSans-ExtraBold'}}>YOUR NEW WEBSITE</h1>
                         <img className="shape6 mdl-fw" src="./img/shapes/rectangle-6.png"/>
                         <p className="newwebcaption" style={{fontFamily: 'Lato-Regular'}}>Let's create something  that will convert  visitors to instant customers.<br/>With clean and clear UI/UX and branded aesthestics, create valuable<br/> qualified leads.</p>
-                        <button className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored getsbutton">
+                        <button className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored getsbutton btn-custom-hover">
                             GET ESTIMATE
                         </button>
                      </div>
@@ -109,20 +103,119 @@ class Main extends Component {
                 </div>
 
                 <div className="mdl-grid" id="product">
-                    <div className="mdl-cell mdl-cell--6-col web">
-                        <img className="photolivelogo mdl-fw" src="./img/logo/photolive-logo.png"/>
-                    </div>
+                    <ul className="bxslider">
+                        <li>
+                            <div className="mdl-grid">
+                                <div className="mdl-cell mdl-cell--6-col">
+                                    <img className="photolivelogo mdl-fw" src="./img/products/photolive.jpg"/>
+                                </div>
+                                <div className="mdl-cell mdl-cell--6-col product-description">
+                                    <img className="product-logos" src="./img/logo/photolive-logo.png"/>
+                                    <p className="photoliveinfo">
+                                        <font style={{ color: '#63d6ef', fontWeight: 'bold' }}>PhotoLive </font>
+                                        is a social media tool and the first digital photo
+                                        booth in the country that allows brands to gain valuable
+                                        presence in top social networking sites like Facebook,
+                                        Instagram, Twitter and email in real time.
+                                    </p>
+                                    <div className="row text-center">
+                                        <a href="http://photolive.com.ph" target="_blank" className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored productbtn btn-custom-hover">
+                                            VISIT SITE
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
 
-                    <div className="mdl-cell mdl-cell--6-col products" >
-                        <label className="products1" style={{fontFamily: 'MerriweatherSans-ExtraBold'}}>Products</label>
-                        <img className="shape666 mdl-fw" src="./img/shapes/rectangle-6.png"/>
-                        <p className="photoliveinfo"><font style={{ color: '#63d6ef'}}>Photolive</font> is a social media tool and the first digital photo<br className="mdl-layout--large-screen-only"/> booth in the country that allow's brands to gain valuable<br className="mdl-layout--large-screen-only"/>presence in top social networking sites like Facebook,<br className="mdl-layout--large-screen-only"/> Instagram, Twitter and Email in real time. </p>
-                        <div className="row text-center">
-                            <button className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored productbtn">
-                                VISIT SITE
-                            </button>
-                        </div>
-                    </div>
+                        <li>
+                            <div className="mdl-grid">
+                                <div className="mdl-cell mdl-cell--6-col">
+                                    <img className="photolivelogo mdl-fw" src="./img/products/medix.jpg"/>
+                                </div>
+                                <div className="mdl-cell mdl-cell--6-col product-description">
+                                    <img className="product-logos" src="./img/products/medix-logo.png" alt=""/>
+                                    <p className="photoliveinfo medix">
+                                        <font style={{ color: '#3fa9f5', fontWeight: 'bold' }}>Medix </font>
+                                        is a cloud-based clinic management software that helps
+                                        healthcare practitioners improve their clinic operations using
+                                        just one quick and easy service. It is a 24/7 accessible manager
+                                        that efficiently organizes appointments and clinic records.
+                                    </p>
+                                    <div className="row text-center">
+                                        <a href="http://medix.ph" target="_blank" className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored productbtn btn-custom-hover">
+                                            VISIT SITE
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div className="mdl-grid">
+                                <div className="mdl-cell mdl-cell--6-col">
+                                    <img className="photolivelogo mdl-fw" src="./img/products/soovy.jpg"/>
+                                </div>
+                                <div className="mdl-cell mdl-cell--6-col product-description">
+                                    <img className="product-logos" src="./img/products/soovy-logo.png" alt=""/>
+                                    <p className="photoliveinfo soovy">
+                                        <font style={{ color: '#f47f20', fontWeight: 'bold' }}>Soovy </font>
+                                        is a geolocation mobile app that lets users access
+                                        exclusive frame souvenirs for their travels, concerts, restos,
+                                        special events or advocacy.
+                                    </p>
+                                    <div className="row text-center">
+                                        <a href="http://soovy.me" target="_blank" className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored productbtn btn-custom-hover">
+                                            VISIT SITE
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div className="mdl-grid">
+                                <div className="mdl-cell mdl-cell--6-col">
+                                    <img className="photolivelogo mdl-fw" src="./img/products/bolooka.jpg"/>
+                                </div>
+                                <div className="mdl-cell mdl-cell--6-col product-description">
+                                    <img className="product-logos" src="./img/products/bolooka-logo.png" alt=""/>
+                                    <p className="photoliveinfo bolooka">
+                                        <font style={{ color: 'black', fontWeight: 'bold' }}>Bolooka </font>
+                                        is an e-commerce platform that provides an
+                                        exclusive marketplace for local merchants and artists
+                                        who create only the best products in the country.
+                                    </p>
+                                    <div className="row text-center">
+                                        <a href="http://bolooka.com" target="_blank" className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored productbtn btn-custom-hover">
+                                            VISIT SITE
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div className="mdl-grid">
+                                <div className="mdl-cell mdl-cell--6-col">
+                                    <img className="photolivelogo mdl-fw" src="./img/products/verify.jpg"/>
+                                </div>
+                                <div className="mdl-cell mdl-cell--6-col product-description">
+                                    <img className="product-logos" src="./img/products/verify-logo.png" alt=""/>
+                                    <p className="photoliveinfo verify">
+                                        <font style={{ color: 'orange', fontWeight: 'bold' }}>Verify </font>
+                                        is a real-time solution to monitor product
+                                        samplings and events using gps technology, live
+                                        streaming and a mobile app that can collects data.
+                                    </p>
+                                    <div className="row text-center">
+                                        <a href="http://project-verify.medix.ph/" target="_blank" className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored productbtn btn-custom-hover">
+                                            VISIT SITE
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
 
                 <div className="mdl-grid feedbackgrid">
@@ -155,6 +248,14 @@ class Main extends Component {
                 </div>
 
                 <div className="mdl-grid consult" id="contacts">
+
+                    <img className="hide-temp shapes rotate polygon22" src="./img/shapes/polygon-2.png"/>
+                    <img className="hide-temp shapes rotate polygon33" src="./img/shapes/polygon-3.png"/>
+                    <img className="hide-temp shapes rotate ellipse22" src="./img/shapes/ellipse-2.png"/>
+                    <img className="hide-temp shapes rotate polygon44" src="./img/shapes/polygon-4.png"/>
+                    <img className="hide-temp shapes rotate rectangle1" src="./img/shapes/rectangle.png"/>
+                    <img className="hide-temp shapes rotate rectangle2" src="./img/shapes/rectangle-2.png"/>
+
                     <div className="mdl-cell mdl-cell--6-col consultation">
                         <label className="constitle" style={{fontFamily: 'MerriweatherSans-ExtraBold'}}>Get a Free Consultation</label>
                         <img className="shape6666 mdl-fw"  src="./img/shapes/rectangle-6.png"/>
@@ -237,7 +338,7 @@ class Main extends Component {
                     <div className="map" id="map"></div>
                     <div className="mdl-cell mdl-cell--12-col cwdmap">
                         <label className="drop" style={{fontFamily: 'MerriweatherSans-ExtraBold'}}>DROP BY OUR OFFICE!</label>
-                        <img className="mapshape"  src="./img/shapes/rectangle-6.png"/>
+                        <hr className="hr-drop"/>
                         <p className="cwdadd"> #79 3BB Building Kamuning Street Corner,<br/>
                         11th Jamboree, Quezon City, 1100 Metro Manila</p>
                     </div>
@@ -248,7 +349,7 @@ class Main extends Component {
                         <label className="worklabel" style={{fontFamily: 'MerriweatherSans-ExtraBold'}}>WORK WITH US TODAY!</label>
                         <p className=""> whatsup@cloudwalkdigital.com</p>
                         <p className="">(02) 962 41 92</p>
-                        <button className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored start">
+                        <button className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored start btn-custom-hover">
                             START YOUR PROJECT
                         </button>
                         <p className="footercopyright"> COPYRIGHT © 2017 CLOUDWALK DIGITAL ALL RIGHTS RESERVED</p>
@@ -256,30 +357,21 @@ class Main extends Component {
 
                      <div  className="mdl-cell mdl-cell--6-col socialmedia" >
                         <div  className="mdl-grid" >
-                            <div  className="mdl-cell mdl-cell--6-col mdl-cell--1-col-phone" >
+                            <div  className="mdl-cell mdl-cell--12-col mdl-cell--4-col-phone" >
                                 <a href="https://www.facebook.com/CloudwalkDigital"><img className="fbicon"  src="./img/sm/fb.png"/></a>
-                            </div>
-                            <div className="mdl-cell mdl-cell--6-col  mdl-cell--3-col-phone tandc">
-                                <label style={{fontFamily: 'Lato-Regular', color: '#fff' }}>TERMS AND CONDITIONS</label>
                             </div>
                         </div>
 
                         <div  className="mdl-grid twitter" >
-                            <div  className="mdl-cell mdl-cell--6-col mdl-cell--1-col-phone" >
+                            <div  className="mdl-cell mdl-cell--12-col mdl-cell--4-col-phone" >
                                 <a href="https://twitter.com/cwdph"><img className="twittericon"  src="./img/sm/twitter.png"/></a>
-                            </div>
-                            <div className="mdl-cell mdl-cell--6-col mdl-cell--3-col-phone blog" >
-                                <label style={{fontFamily: 'Lato-Regular', color: '#fff'}}>BLOG</label>
                             </div>
                         </div>
 
                         <div  className="mdl-grid youtube" >
-                            <div  className="mdl-cell mdl-cell--6-col mdl-cell--1-col-phone" >
+                            <div  className="mdl-cell mdl-cell--12-col mdl-cell--4-col-phone" >
                                 <a href="https://www.youtube.com/channel/UCtNG3LyQc_ShGwSWn-8DDhQ"><img className="youtubeicon"  src="./img/sm/youtube.png"/></a>
                             </div>
-                            <div className="mdl-cell mdl-cell--6-col mdl-cell--3-col-phone footercontact">
-                                <label style={{fontFamily: 'Lato-Regular', color: '#fff'}}>CONTACT</label>
-                             </div>
                         </div>
                     </div>
                 </div>
